d65791bfe336aaecd0c9739ec1c056e56d58fe97
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-08-15 20:45:43 -0300

    Added middleware

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UtilsService } from 'src/utils/utils.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Permission } from './permissions/entities/permission.entity';
+import { CheckAuthenticatesMiddleware } from 'src/middlewares/CheckAuthenticates.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Permission])],
   controllers: [UsersController],
   providers: [UsersService, UtilsService],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CheckAuthenticatesMiddleware)
+      .forRoutes({ path: 'users', method: RequestMethod.GET });
+  }
+}
