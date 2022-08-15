f85d623337b8d6687175d53d808c6664d600961d
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-08-15 20:46:13 -0300

    Update properties.module.ts

--- src/properties/properties.module.ts
+++ src/properties/properties.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Property } from './entities/property.entity';
 import { AddressModule } from './address/address.module';
 import { AddressService } from './address/address.service';
+import { Address } from './address/entities/address.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Property]), AddressModule],
+  imports: [TypeOrmModule.forFeature([Property, Address]), AddressModule],
   controllers: [PropertiesController],
   providers: [PropertiesService, AddressService],
 })
