eb367ba6b3f815ed82c824f61a8c504769a9153a
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-08-18 19:43:05 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(), {
+    logger: ['error', 'warn', 'log'],
+  });
   await app.listen(3333);
 }
 bootstrap();
