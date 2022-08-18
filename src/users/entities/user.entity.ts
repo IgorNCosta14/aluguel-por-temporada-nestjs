135246cd415416d3a3eb6664e0030909c3adf3d9
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-08-18 20:00:31 -0300

    Added column options

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -12,19 +12,19 @@ import { Permission } from '../permissions/entities/permission.entity';
 
 @Entity('users')
 export class User {
-  @PrimaryColumn('uuid')
+  @PrimaryColumn({ type: 'uuid', unique: true })
   id: string;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: false })
   name: string;
 
-  @Column({ length: 14 })
+  @Column({ length: 14, nullable: false })
   cpf: string;
 
-  @Column({ length: 50 })
+  @Column({ length: 50, nullable: false })
   email: string;
 
-  @Column({ length: 20 })
+  @Column({ length: 20, nullable: false })
   password: string;
 
   @Column({ type: 'tinyint' })
