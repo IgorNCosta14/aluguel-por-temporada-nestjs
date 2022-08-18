fa7d1656505946ab59a1ed3551cb5fe1b929a059
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-08-18 20:02:12 -0300

    Added middleware

--- src/properties/properties.module.ts
+++ src/properties/properties.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { PropertiesService } from './properties.service';
 import { PropertiesController } from './properties.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,10 +6,24 @@ import { Property } from './entities/property.entity';
 import { AddressModule } from './address/address.module';
 import { AddressService } from './address/address.service';
 import { Address } from './address/entities/address.entity';
+import { CheckAuthenticatesMiddleware } from 'src/middlewares/CheckAuthenticates.middleware';
+import { UsersService } from 'src/users/users.service';
+import { User } from 'src/users/entities/user.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Property, Address]), AddressModule],
+  imports: [
+    TypeOrmModule.forFeature([Property, Address, User]),
+    AddressModule,
+    UsersModule,
+  ],
   controllers: [PropertiesController],
-  providers: [PropertiesService, AddressService],
+  providers: [PropertiesService, AddressService, UsersService],
 })
-export class PropertiesModule {}
+export class PropertiesModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CheckAuthenticatesMiddleware)
+      .forRoutes({ path: 'properties', method: RequestMethod.ALL });
+  }
+}
