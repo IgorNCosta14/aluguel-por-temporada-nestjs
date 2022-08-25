a39f348c9b78d37e58c55126017692663704ff17
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-08-25 19:29:31 -0300

    Added check landlord middleware

--- src/rentals/rentals.module.ts
+++ src/rentals/rentals.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { RentalsService } from './rentals.service';
 import { RentalsController } from './rentals.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -9,6 +9,9 @@ import { PropertiesService } from 'src/properties/properties.service';
 import { PropertiesModule } from 'src/properties/properties.module';
 import { UsersModule } from 'src/users/users.module';
 import { Property } from 'src/properties/entities/property.entity';
+import { CheckAuthenticatesMiddleware } from 'src/middlewares/CheckAuthenticates.middleware';
+import { UsersService } from 'src/users/users.service';
+import { CheckLandLordMiddleware } from 'src/middlewares/CheckLandLord.middleware';
 
 @Module({
   imports: [
@@ -17,6 +20,19 @@ import { Property } from 'src/properties/entities/property.entity';
     UsersModule,
   ],
   controllers: [RentalsController],
-  providers: [RentalsService, PropertiesService, DateProviderService],
+  providers: [
+    RentalsService,
+    PropertiesService,
+    DateProviderService,
+    UsersService,
+  ],
 })
-export class RentalsModule {}
+export class RentalsModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CheckAuthenticatesMiddleware).forRoutes(RentalsController);
+    consumer.apply(CheckLandLordMiddleware).forRoutes({
+      path: 'rentals/devolution/',
+      method: RequestMethod.PATCH,
+    });
+  }
+}
