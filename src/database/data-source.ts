a7987a5563fa224db2398945df35695ca677d1a0
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-08-30 22:02:33 -0300

    Update data-source.ts

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,10 +1,13 @@
 import { DataSource } from 'typeorm';
 
 export const AccountDataSource = new DataSource({
-  type: 'sqlite',
-  database: 'dist/db.sqlite',
-  synchronize: true,
-  logging: true,
-  entities: ['dist/**/*.entity.{js,ts}'],
+  type: 'postgres',
+  port: 5432,
+  host: 'postgres',
+  username: 'aluguel_nest',
+  password: 'aluguel_nest',
+  database: 'aluguel_nest',
   migrations: ['dist/database/migrations/**/*.{js,ts}'],
+  entities: ['dist/**/*entity.{js,ts}'],
+  synchronize: false,
 });
