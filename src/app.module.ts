72a8870e386377c298a0e11da6e48b6a5ae19e25
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-09-02 19:51:42 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
-import { Permission } from './users/permissions/entities/permission.entity';
 import { PropertiesModule } from './properties/properties.module';
-import { Address } from './properties/address/entities/address.entity';
-import { Property } from './properties/entities/property.entity';
 import { RentalsModule } from './rentals/rentals.module';
 import { DateProviderService } from './utils/date-provider/date-provider.service';
-import { Rental } from './rentals/entities/rental.entity';
 import { PermissionsModule } from './users/permissions/permissions.module';
 
 @Module({
@@ -24,13 +19,6 @@ import { PermissionsModule } from './users/permissions/permissions.module';
       entities: ['dist/**/*entity.{js,ts}'],
       synchronize: false,
     }),
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: __dirname + '/db.sqlite',
-    //   synchronize: true,
-    //   logging: true,
-    //   entities: [User, Permission, Address, Property, Rental],
-    // }),
     UsersModule,
     PermissionsModule,
     PropertiesModule,
