b870defbee5e71c6d0b14a59d4349c18c205a48c
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-09-05 19:24:19 -0300

    updated

--- src/users/permissions/permissions.module.ts
+++ src/users/permissions/permissions.module.ts
@@ -1,13 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { PermissionsController } from './permissions.controller';
 import { Permission } from './entities/permission.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CheckAuthenticatesMiddleware } from 'src/middlewares/CheckAuthenticates.middleware';
+import { UsersModule } from '../users.module';
+import { UsersService } from '../users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Permission])],
+  imports: [TypeOrmModule.forFeature([Permission]), UsersModule],
   controllers: [PermissionsController],
-  providers: [PermissionsService],
+  providers: [PermissionsService, UsersService],
 })
 export class PermissionsModule {
   // configure(consumer: MiddlewareConsumer) {
